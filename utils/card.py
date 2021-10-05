999ae92b721cfbd5b421ae3a84720c81550b8eab
PhilippeJanssens <issajanssens@gmail.com>
2021-10-05 17:03:09 +0200

    work in process at Player and Deck

--- utils/card.py
+++ utils/card.py
@@ -2,19 +2,53 @@ icons = ["♥", "♦", "♣", "♠"]
 values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
 
-class Symbol():
-    def __init__(self, color, icon):
-        self.color = color
+class Symbol:
+    """Class representing one symbol"""
+
+    def __init__(self, icon, color):
+        """Constructor of the class Symbol"""
         self.icon = icon
+        self.color = color
+
     def __str__(self):
-        return
+        """Method called during a conversion of the object
+        into a chain"""
+        return "{}-{}".format(self.icon, self.color)
+
+
+"""
+CHECK class SYMBOL
+symbol = Symbol("♥", "red")
+print(type(symbol), symbol.color, symbol.icon)
+print(symbol.__str__())
+"""
+
 
 class Card(Symbol):
-    def __init__(self, symbol, value):
-        super().__init__()
+    """
+    A class that defines a card.
+    It inherits from the class Symbol.
+    """
+
+    def __init__(self, icon, value):
+        """A card is defined by its value and icon."""
+        super().__init__(icon, value)
+        self.icon = icon
+        self.value = value
 
     def __str__(self):
-        return
+        """Method called during a conversion of the object
+        into a chain"""
+        return "{} {}".format(self.icon, self.value)
 
-    value=""
 
+# CHECK class CARD
+"""card = Card("♥", "7")
+print(type(card), card.color, card.icon)
+print(card.__str__())
+
+print(card.__str__())
+print(card)
+print(str(card))
+print(card.__repr__())
+"""
--- utils/player.py
+++ utils/player.py
@@ -1,43 +1,90 @@
-from card.py import Card
-from random import randint
+import random
+from card import Card
+
+var_testing = Card("♥", "7")
+print(var_testing)
+print(var_testing.icon)
+
+
+cards = ['A of ♥', '2 of ♥', '3 of ♥', '4 of ♥', '5 of ♥', '6 of ♥', '7 of ♥', '8 of ♥', '9 of ♥', '10 of ♥', 'J of ♥',
+         'Q of ♥', 'K of ♥', 'A of ♦', '2 of ♦', '3 of ♦', '4 of ♦', '5 of ♦', '6 of ♦', '7 of ♦', '8 of ♦', '9 of ♦',
+         '10 of ♦', 'J of ♦', 'Q of ♦', 'K of ♦', 'A of ♣', '2 of ♣', '3 of ♣', '4 of ♣', '5 of ♣', '6 of ♣', '7 of ♣',
+         '8 of ♣', '9 of ♣', '10 of ♣', 'J of ♣', 'Q of ♣', 'K of ♣', 'A of ♠', '2 of ♠', '3 of ♠', '4 of ♠', '5 of ♠',
+         '6 of ♠', '7 of ♠', '8 of ♠', '9 of ♠', '10 of ♠', 'J of ♠', 'Q of ♠', 'K of ♠']
+history = []
 
 class Player:
 
-    def __init__(self, cards, turn_count, number_of_cards, history):
-        return
+    def __init__(self, card, turn_count, number_of_cards, history):
+        self.card = card
+        self.turn_count = turn_count
+        self.number_of_cards = number_of_cards
+        self.history = history
+
+        self.rnd_card_pick = ""
+        self.player_name = "Player " + str(turn_count)
 
     def __str__(self):
-        return
+        return "{} {} {} {}".format(self.card, self.turn_count, self.number_of_cards, self.history)
 
-    def play(self):
+    def play(self, player_name, turn_count, card_number, card_symbol_icon):
         # randomly pick a card
 
+        rnd_card_pick = cards[random.randrange(0, 52)]
         # add the card to the player's history
-
+        history.append(rnd_card.pick)
         # print {player_name} {turn_count} played: {card_number} {card_symbol_icon}.
 
-        # return the card
+        return "{} {} played: {} {}".format(player_name, turn_count, card_number, card_symbol_icon)
 
 
+# CHECK class PLAYER
+player = Player(7, cards[0], 5, ["♥ of 10", "♥ of K", "♥ of 4"])
+print(player)
+print(type(player), player.card, player.turn_count, player.number_of_cards, player.history)
+print(player.__str__())
 
+# player = Player(7, cards[0], 5, ["♥ of 10", "♥ of K", "♥ of 4"])
+# play()
+# print(x)
+# print(type(player), player.card, player.turn_count, player.number_of_cards, player.history)
+# print(player.__str__())
+
+"""
 class Deck:
-    def __init__(self, cards):
-        self.cards = []
-        self.build_a_deck()
 
-    def __str__(self):
+    icons = ["♥", "♦", "♣", "♠"]
+    values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+    def __init__(self, cards):
+        self.cards = cards[]
         return
 
+    def __str__(self):
+        deck = ''
+        for card in self.deck:
+            deck += '\n ' + card.__str__()
+        return "All of our cards:" + deck
+
+
     def fill_deck():
-        # configure all the 52 cards
+        for icon in icons:
+            for value in values:
+                self.cards.append(Card(icon, value))
+        return
 
-    def shuffle():
-        # shuffle
+    def shuffle(self):
+        random.shuffle(cards)
+        return
 
-    def distribute():
+    def distribute(self, play_list):
+        self.play_list = play_list[]
         # this will take a list of player as parameter
         # end will distribute the cards evenly all the players
+        return
 
-
+fill_deck()
 deck = Deck()
 deck.display()
+
+"""
